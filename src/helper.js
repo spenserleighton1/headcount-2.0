3534a6815b112a249f7b0f844c6dbd4fcf614514
Spenser Leighton <spenser.leighton@gmail.com>
2018-06-26 17:12:43 -0600

    Finish iteration 1 tests

--- src/helper.js
+++ src/helper.js
@@ -7,23 +7,44 @@ export default class DistrictRepository {
 
 dataCleaner = (data) => {
     let dataObj = data.reduce((dataObj, district) => {
-      let location = district.Location.toUpperCase()
-        if(!dataObj[location]) {
-          dataObj[location] = [district]
-        } else {
-          dataObj[location].push(district)
+
+      let location = district.Location.toUpperCase();
+      let year = district.TimeFrame
+      let stat = Math.round(district.Data * 1000)/1000;
+      if(!stat) {
+        stat = 0
+      }
+      let correctStats = {[year]: stat}
+
+      if(!dataObj[location]) {
+        dataObj[location] = {
+          location,
+          stats: {}
         }
+      } 
+      dataObj[location].stats = {...dataObj[location].stats, ...correctStats}
+       
       return dataObj
     },{})
-    return dataObj
+    return  dataObj
   }
 
-  findByName = (str) => {
-    Object.keys(this.stats).find(district => {
-      return district === str
-    })
-
+findByName = (str) => {
+  if (!str) {
+    return
   }
+  let cleanStr = str.toUpperCase()
+    return this.stats[cleanStr]
+  }
+
+findAllMatches = (str = '') => {
+  str = str.toUpperCase()
+  return Object.values(this.stats).filter(district => {
+    return district.location.includes(str)
+  })
+}
+
+
 }
 
 
--- src/test/unit/iteration-1-part2.test.js
+++ src/test/unit/iteration-1-part2.test.js
@@ -4,15 +4,15 @@ import kinderData from '../../data/kindergartners_in_full_day_program.js';
 describe('DistrictRepository iteration 1 - part 2', () =>  {
   const district = new DistrictRepository(kinderData);
 
-  it.skip('findAllMatches defaults to returning all data in an array', () => {
+  it('findAllMatches defaults to returning all data in an array', () => {
     expect(district.findAllMatches().length).toBe(181);
   });
 
-  it.skip('findAllMatches returns matches in an array, case insensitive', () => {
+  it('findAllMatches returns matches in an array, case insensitive', () => {
     expect(district.findAllMatches('ColoRado').length).toBe(2);
   });
 
-  it.skip('findAllMatches finds no matches and returns an empty array when given arguments that dont exist within the data', () => {
+  it('findAllMatches finds no matches and returns an empty array when given arguments that dont exist within the data', () => {
     expect(district.findAllMatches('al;dkfjas;dlkjasdfl;').length).toBe(0);
     expect(district.findAllMatches('packers').length).toBe(0);
     expect(district.findAllMatches('df').length).toBe(0);
