a2cc34e3b0ad771cbd24ef44790f513698bd8d1b
Keeliana Marler <kmmarler2@hotmail.com>
2018-06-27 15:07:14 -0600

    add conditional color rendering

--- src/Card/index.js
+++ src/Card/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import './styles.css'
 
 const Card = (props) => {
   // console.log(props)
   return(
-    <div>
+    <div className='card'>
     <h3>{props.location}</h3>
     {Object.keys(props.stats).map((stat, index) => {
-      return <p key={index}>{stat}: {props.stats[stat]}</p>})}
+      return <p 
+              key={index}
+              style={props.stats[stat] >= 0.5 ? {color: 'red'} : {color: 'green'}}>{stat}: {props.stats[stat]}</p>})}
     </div>
     )
 }
