41934da30606fae4c2865ef3440f8ceeb380769d
Keeliana Marler <kmmarler2@hotmail.com>
2018-07-01 10:57:01 -0600

    add app tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,16 +2,81 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { shallow } from 'enzyme';
+import DistrictRepository from './helper.js'
+import kinderData from './data/kindergartners_in_full_day_program.js';
+
 
 describe('app', () => {
-  let wrapper;
-  beforeEach(() => {
-    wrapper=shallow(<App />)
-  })
-  it('should have an districtData object in state', () => {
-    const expectedState = {};
-    const actualState = wrapper.state('districtData')
+  let renderedApp;
+  let mockData;
+  let district = new DistrictRepository(kinderData);
 
-    expect(actualState).to
+    beforeEach(() => {
+    mockData = {
+             '2004': 0.24,
+              '2005': 0.278,
+              '2006': 0.337,
+              '2007': 0.395,
+              '2008': 0.536,
+              '2009': 0.598,
+              '2010': 0.64,
+              '2011': 0.672,
+              '2012': 0.695,
+              '2013': 0.703,
+              '2014': 0.741 }
+
+    renderedApp = shallow(renderedApp=(<App />));
   })
+
+  it('should exist', () => {
+    expect(renderedApp).toBeDefined()
+  });
+
+  it('should match the snapshot', () => {
+    expect(renderedApp).toMatchSnapshot()
+  }) 
+
+  it('should have the correct default state', () => {
+    let expected = {
+      districtData: district.dataCleaner(kinderData),
+      comparedDistricts: {}
+    }
+    let actual = renderedApp.state();
+    expect(actual).toEqual(expected)
+  })
+
+  it('should add an object for each district', () => {
+      renderedApp.setState({
+        districtData: district.dataCleaner(kinderData),
+        comparedDistricts: {}, 
+      })
+      let expected = 181;
+      let actual = Object.keys(district.dataCleaner(kinderData)).length
+      expect(actual).toEqual(expected);
+  })
+
+  it('when comparing cards, should render the original cards, the selected cards, and a comparison card', () => {
+    
+  })
+
+  it('should only add two selected cards', () => {
+
+  })
+
+  it('should remove a card from the unselected cards list when clicked once', () => {
+
+  })
+
+  it('should remove a card from the selected cards list when clicked twice', () => {
+
+  })
+
+  it('should add a comparison card when clicked twice', () => {
+
+  })
+
+  it('should remove a card from the selected cards list when clicked twice', () => {
+
+  })
+
 })
