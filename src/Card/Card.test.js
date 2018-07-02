4ba19da56137b696559e1ab2a3c15d398b71f6e1
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-02 11:07:28 -0600

    Lint card and card test files

--- src/Card/Card.test.js
+++ src/Card/Card.test.js
@@ -5,45 +5,41 @@ import { shallow } from 'enzyme';
 describe('Card', () => {
   let wrapper;
   let mockData;
-  let mockFn
+  let mockFn;
 
   beforeEach(() => {
     mockData = {
-             '2004': 0.24,
-              '2005': 0.278,
-              '2006': 0.337,
-              '2007': 0.395,
-              '2008': 0.536,
-              '2009': 0.598,
-              '2010': 0.64,
-              '2011': 0.672,
-              '2012': 0.695,
-              '2013': 0.703,
-              '2014': 0.741 }
+      '2004': 0.24,
+      '2005': 0.278,
+      '2006': 0.337,
+      '2007': 0.395,
+      '2008': 0.536,
+      '2009': 0.598,
+      '2010': 0.64,
+      '2011': 0.672,
+      '2012': 0.695,
+      '2013': 0.703,
+      '2014': 0.741 };
 
-    mockFn = jest.fn()          
-    wrapper = shallow(<Card location={ 'COLORADO' }
-                            stats={ mockData }
-                            findDistrictByClick={ mockFn }
-                            selected={ false } />)
-  })
+    mockFn = jest.fn()   ;       
+    wrapper = shallow(<Card 
+      location={ 'COLORADO' }
+      stats={ mockData }
+      findDistrictByClick={ mockFn }
+      selected={ false } />);
+  });
 
   it('matches the Snapshot', () => {
-    expect(wrapper).toMatchSnapshot()
-  })
+    expect(wrapper).toMatchSnapshot();
+  });
 
   it('should call the findDistrictByClick function when the card is clicked one', () => {
-    wrapper.find('.card').simulate('click')
+    wrapper.find('.card').simulate('click');
     expect(mockFn).toBeCalled();
-  })
+  });
 
   it('should have a className of selected set at default to false', () => {
     expect(wrapper.is('.selected')).toEqual(false);
-  })
+  });
 
-  // it('should have a className selected set as true when findDistrictByClick is called', () => {
-  //   wrapper.find('.card').simulate('click')
-  //   expect(wrapper.is('selected')).toEqual(true);
-  // })
-
-})
+});
--- src/Card/index.js
+++ src/Card/index.js
@@ -1,27 +1,33 @@
 import React from 'react';
-import './styles.css'
+import './styles.css';
 import PropTypes from 'prop-types';
 
 
 const Card = (props) => {
-  return(
-    <div className={`card ${props.select ? ' select' : ''}`}
-         onClick={()=> { props.findDistrictByClick(props.location) }}>
-    <h3>{props.location}</h3>
-    <ul>
-    {Object.keys(props.stats).map((stat, i) => {
-      return <li 
-              key={i}
-              style={props.stats[stat] >= 0.5 ? {color: '#fff'} : {color: '#1D3557'}}>
-              {stat}: {props.stats[stat]}
-              </li>})}
-    </ul>
+  return (
+    <div 
+      className={`card ${props.select ? ' select' : ''}`}
+      onClick={()=> { props.findDistrictByClick(props.location); }}>
+      <h3>{props.location}</h3>
+      <ul>
+        {Object.keys(props.stats).map((stat, index) => {
+          return <li 
+            key={index}
+            style={props.stats[stat] >= 0.5 ? {color: '#fff'} : {color: '#1D3557'}}>
+            {stat}: {props.stats[stat]}
+          </li>; 
+        })
+        }
+      </ul>
     </div>
-    )
-}
+  );
+};
 
 Card.propTypes = {
-  props: PropTypes.object.isRequired
-}
+  select: PropTypes.string.isRequired,
+  findDistrictByClick: PropTypes.func.isRequired,
+  location: PropTypes.string.isRequired,
+  stats: PropTypes.object.isRequired
+};
 
 export default Card;
